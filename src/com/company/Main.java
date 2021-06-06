8da95e19057d370c3c49370757207bc6b814ed34
vasanthmaharshi <y.vasanthmaharshi@gmail.com>
2021-06-06 19:50:03 +0530

    test 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws java.lang.Exception{
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a+b);
+    }
+}
